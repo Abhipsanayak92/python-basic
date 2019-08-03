ee61a276643552c7e1fb534b0109743c2d799eb8
Abhipsanayak92 <49823894+Abhipsanayak92@users.noreply.github.com>
2019-08-03 13:32:37 +0530

    Add files via upload

---
+++ 1 arithmatic_function.py
@@ -0,0 +1,63 @@
+# -*- coding: utf-8 -*-
+"""
+Spyder Editor
+
+This is a temporary script file.
+"""
+#%% 
+#*Comment code*#
+
+#Single line comment
+""" Multi line comment"""
+
+#%%  
+#*arithmatic function code*#
+a=5; d=10
+b, c=3.2, "Hello"
+x = y = z = "World"
+print("Value of a=\n",a)  # \n for new line
+print("Value of b=",b)
+print()  # only for space
+print(b,c)
+print(b,c,sep=",")
+print(c) 
+c #if this is the last line in a block, thenit will pint hello
+#but if something is there after this line, python will ignore this line
+print(a)
+c
+#%%
+#* Division operation in python *#
+print(1/3)
+
+print(1//3)  #floored division
+
+print(7%3) # modulo division #display the reminder value after division
+
+print(6%3)
+
+
+#%%
+#Data type
+a= 5
+b= 3.2
+print(type(b))
+print(a+b)
+
+a="5"
+b= "3.2"
+print(a+b)
+print(type(b))
+
+#%%
+#User input
+name = input("Enter your name: ")
+print("Hello " , name)
+
+#%%
+
+
+
+
+
+
+
